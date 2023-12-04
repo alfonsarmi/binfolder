fa0b0885a7401cc69b3a2881499f72dea8130f0c
Alfonso Sarmiento Ráez <asarmiento@foreworth.com>
2023-12-04 15:09:24 +0100

    adsfasf

---
+++ test/Bin/ActionModel - Copy.cs
@@ -0,0 +1,69 @@
+﻿using Clonator.Helpers;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Clonator.Models
+{
+    public class ActionModel
+    {        
+        public virtual string? Action { get; set; }
+        public string? OrigEnvironment { get; set; }
+        public string? DestEnvironment { get; set; }
+        
+
+        private string _destTenant;
+        public string DestTenant
+        {
+            get
+            {
+                return this._destTenant;
+            }
+            set
+            {
+                this._destTenant = value?.Replace("_", ".")?.ToLower();
+            }
+        }
+
+        private string _origTenant;
+        public string OrigTenant
+        {
+            get
+            {
+                return this._origTenant;
+            }
+            set
+            {
+                this._origTenant = value?.Replace("_", ".")?.ToLower();
+            }
+        }
+
+        public virtual async Task Run(
+            Stopwatch sw,
+            ConnectionModel origConnection,
+            ConnectionModel destConnection)
+        {            
+            Logger.Log($"No valid action found.");
+
+            // This method should stay empty
+            // Following statement will prevent a compiler warning:
+            await Task.FromResult(0);
+        }
+
+        public virtual async Task Run(
+            Stopwatch sw,
+            ConnectionModel destConnection)
+        {
+            Logger.Log($"No valid action found.");
+
+            // This method should stay empty
+            // Following statement will prevent a compiler warning:
+            await Task.FromResult(0);
+        }
+
+        public virtual bool IsJustDestinationAction()
+        {           
+            return false;
+        }
+
+    }
+}
